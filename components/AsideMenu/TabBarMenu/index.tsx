3526db642bd3400414d6b095deb8e98d2e6cb555
Renatogranados <renatogranadosgt@gmail.com>
2022-08-10 11:06:16 -0600

    feat: add all sections to aside menu

--- components/AsideMenu/TabBarMenu/index.tsx
+++ components/AsideMenu/TabBarMenu/index.tsx
@@ -49,9 +49,9 @@ export default function TabBarMenu() {
             </p>
             <LogIn />
           </section>
-          <span className={styles.separator}></span>
 
           {/* Explorar */}
+          <span className={styles.separator}></span>
           <section className={styles.exploreContainer}>
             <p className={styles.explorerTitle}>EXPLORAR</p>
             <ul className={styles.ul}>
@@ -81,6 +81,118 @@ export default function TabBarMenu() {
               </MenuItem>
             </ul>
           </section>
+
+          {/* Explorar canales */}
+          <span className={styles.separator}></span>
+          <section>
+            <ul className={styles.ul}>
+              <MenuItem url={'/more-youtube'}>
+                <img className={styles.circlePlus} src='/icons/circle-plus.svg' alt='' />
+                <p className={styles.exploreLabel}>Explorar canales</p>
+              </MenuItem>
+            </ul>
+          </section>
+
+          {/* Más de youtube */}
+          <span className={styles.separator}></span>
+          <section className={styles.moreYoutubeContainer}>
+            <p className={styles.explorerTitle}>MÁS DE YOUTUBE</p>
+            <ul className={styles.ul}>
+              <MenuItem url={'/premium'}>
+                <img className={styles.moreYoutubeImage} src='/images/youtube-logo.png' alt='' />
+                <p className={styles.exploreLabel}>YouTube Premium</p>
+              </MenuItem>
+
+              <MenuItem url={'/youtube-music'}>
+                <img className={styles.moreYoutubeImage} src='/images/youtube-music.png' alt='' />
+                <p className={styles.exploreLabel}>YouTube Music</p>
+              </MenuItem>
+
+              <MenuItem url={'/youtube-kids'}>
+                <img className={styles.moreYoutubeImage} src='/images/youtube-kids.png' alt='' />
+                <p className={styles.exploreLabel}>YouTube Kids</p>
+              </MenuItem>
+
+              <MenuItem url={'/youtube-tv'}>
+                <img className={styles.moreYoutubeImage} src='/images/youtube-tv.png' alt='' />
+                <p className={styles.exploreLabel}>YouTube TV</p>
+              </MenuItem>
+            </ul>
+          </section>
+
+          {/* Configuraciones */}
+          <span className={styles.separator}></span>
+          <section className={styles.exploreContainer}>
+            <ul className={styles.ul}>
+              <MenuItem url={'/music'}>
+                <img className={styles.configImage} src='/icons/settings.svg' alt='' />
+                <p className={styles.exploreLabel}>Configuración</p>
+              </MenuItem>
+
+              <MenuItem url={'/sports'}>
+                <img className={styles.configImage} src='/icons/flag-regular.svg' alt='' />
+                <p className={styles.exploreLabel}>Deportes</p>
+              </MenuItem>
+
+              <MenuItem url={'/videogames'}>
+                <img className={styles.configImage} src='/icons/help.svg' alt='' />
+                <p className={styles.exploreLabel}>Videojuegos</p>
+              </MenuItem>
+
+              <MenuItem url={'/learning'}>
+                <img className={styles.configImage} src='/icons/comments.svg' alt='' />
+                <p className={styles.exploreLabel}>Aprendizaje</p>
+              </MenuItem>
+            </ul>
+          </section>
+
+          {/* Footer info */}
+          <span className={styles.separator}></span>
+          <footer className={styles.footerInfoContainer}>
+            <div className={styles.footerLinksContainer}>
+              <a className={styles.footerAnchor} href='#acerca-de'>
+                Acerca de
+              </a>
+              <a className={styles.footerAnchor} href='#prensa'>
+                Prensa
+              </a>
+              <a className={styles.footerAnchor} href='#derechos'>
+                Derechos de autor
+              </a>
+              <a className={styles.footerAnchor} href='#comunicacion'>
+                Comunicarte con nosotros
+              </a>
+              <a className={styles.footerAnchor} href='#creadores'>
+                Creadores
+              </a>
+              <a className={styles.footerAnchor} href='#anunciar'>
+                Anunciar
+              </a>
+              <a className={styles.footerAnchor} href='#develop'>
+                Desarrolladores
+              </a>
+            </div>
+            <div className={styles.footerLinksContainer}>
+              <a className={styles.footerAnchor} href='#condiciones'>
+                Condiciones
+              </a>
+              <a className={styles.footerAnchor} href='#Privacidad'>
+                Privacidad
+              </a>
+              <a className={styles.footerAnchor} href='#politicas'>
+                Políticas y seguridad
+              </a>
+              <a className={styles.footerAnchor} href='#howItWors'>
+                Cómo funciona YouTube
+              </a>
+              <a className={styles.footerAnchor} href='#betatester'>
+                Prueba funciones nuevas
+              </a>
+            </div>
+            <div className={styles.footerLinksContainer}>
+              <span className={styles.copyright}>© 2022 Google LLC</span>
+            </div>
+          </footer>
         </>
       )}
     </ul>
